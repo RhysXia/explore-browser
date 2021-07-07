b59a53c36c2fc0d107d088f8e7becaccdea0f280
Rhys Xia <xrs4433@outlook.com>
2021-07-07 23:11:57 +0800

    feat: update

--- typings/next.d.ts
+++ typings/next.d.ts
@@ -1,13 +1,10 @@
 import { ApolloClient } from "@apollo/client";
+import { Router } from "next/dist/client/router";
+import { AppContextType } from "next/dist/next-server/lib/utils";
 import { AppStore } from "./lib/redux";
-
-declare module 'next/dist/next-server/lib/utils' {
-  type AppContextType<R extends NextRouter = NextRouter> = {
-    Component: NextComponentType<NextPageContext>;
-    AppTree: AppTreeType;
-    ctx: NextPageContext;
-    router: R;
-    reduxStore: AppStore
-    apolloClient: ApolloClient
-  };
+declare module "next/app" {
+  interface AppContext extends AppContextType<Router> {
+    reduxStore: AppStore;
+    apolloClient: ApolloClient;
+  }
 }
