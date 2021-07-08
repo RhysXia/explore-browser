e81a5d0b3722e6f1c6145322a29c2ccddc5a76e8
xiaruisong <xiaruisong.kuaishou.com>
2021-07-08 20:44:13 +0800

    feat: update

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,13 @@ import Header from "../components/Header";
 import React from "react";
 import Footer from "../components/Footer";
 import { AppPageContext } from "../typings/next";
+import { useAppSelector } from "../lib/redux";
 
 const Home: NextComponentType<AppPageContext> = (props) => {
+  const token = useAppSelector((store) => store.token);
+
+  console.log(token);
+
   return (
     <div>
       <Head>
@@ -18,10 +23,9 @@ const Home: NextComponentType<AppPageContext> = (props) => {
 };
 
 Home.getInitialProps = async (ctx) => {
-
   return {
-    pageProps: {}
-  }
+    a: 1,
+  };
 };
 
 export default Home;
