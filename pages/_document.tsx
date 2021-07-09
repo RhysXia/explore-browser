b9d4b7728c4a23d0505ebc13f5a77c90e63aba8f
RhysXia <xrs4433@outlook.com>
2021-07-09 09:30:17 +0800

    feat: 接入xl-vision

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,6 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
-
-export default class extends Document {
+import { ServerStyleSheet } from "styled-components";
+class MyDocument extends Document {
   render() {
     return (
       <Html>
@@ -16,3 +16,30 @@ export default class extends Document {
     );
   }
 }
+
+MyDocument.getInitialProps = async (ctx) => {
+  const sheet = new ServerStyleSheet();
+  const originalRenderPage = ctx.renderPage;
+
+  try {
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+      });
+
+    const initialProps = await Document.getInitialProps(ctx);
+    return {
+      ...initialProps,
+      styles: (
+        <>
+          {initialProps.styles}
+          {sheet.getStyleElement()}
+        </>
+      ),
+    };
+  } finally {
+    sheet.seal();
+  }
+};
+
+export default MyDocument;
