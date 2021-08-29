05be1b56cf12c47a66cf18461b0856de50f35b70
Rhys Xia <xrs4433@outlook.com>
2021-08-29 23:18:51 +0800

    feat: update

---
+++ components/editor/Editor.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { createEditor, Descendant } from "slate";
+import { Slate, Editable, withReact } from "slate-react";
+
+export type EditorProps = {
+  initialValue?: Array<Descendant>;
+  onChange?: (value: Array<Descendant>) => void;
+  placeholder?: string;
+};
+
+const DEFAULT_VALUE: Array<Descendant> = [
+  {
+    type: "paragraph",
+    children: [{ text: "A line of text in a paragraph." }],
+  },
+];
+
+const Editor: React.FunctionComponent<EditorProps> = (props) => {
+  const { initialValue = DEFAULT_VALUE, onChange, placeholder } = props;
+
+  const [value, setValue] = React.useState(initialValue);
+
+  React.useEffect(() => {
+    onChange?.(value);
+  }, [value, onChange]);
+
+  const editor = React.useMemo(() => withReact(createEditor()), []);
+
+  return null;
+};
+
+export default Editor;
